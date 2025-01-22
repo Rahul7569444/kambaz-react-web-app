ddb6162f2d8251ccaf7ebd6f498e509b2807bec7
gollarahul091 <golla.ra@northeastern.edu>
2025-01-22 14:51:05 -0500

    Update

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -3,7 +3,7 @@ import { Navigate, Route, Routes } from "react-router";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
-import AssignmentEditor from "./Assignments/editor";
+import AssignmentEditor from "./Assignments/Editor";
 
 export default function Courses() {
   return (
