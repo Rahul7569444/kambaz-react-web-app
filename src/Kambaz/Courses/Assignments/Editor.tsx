c3a9051e7746643bd2ebcd5fa4d0fd08d70a8a14
gollarahul091 <golla.ra@northeastern.edu>
2025-04-06 12:24:05 -0400

    Add frontend code

--- src/Kambaz/Courses/Assignments/Editor.tsx
+++ src/Kambaz/Courses/Assignments/Editor.tsx
@@ -1,23 +1,24 @@
 import { useParams, useNavigate } from "react-router";
-import { Link} from "react-router-dom";
-//import * as db from "../../Database";
-import { addAssignment, updateAssignment, setAssignment } from "./reducer";
+import { Link } from "react-router-dom";
+import { addAssignment, setAssignment, updateAssignment } from "./reducer";
 import { useDispatch, useSelector } from "react-redux";
-//import { useState } from "react";
+import * as assignmentClient from "./client";
 
 export default function AssignmentEditor() {
   const { aid } = useParams();
   const { cid } = useParams();
-  //const assignment = db.assignments;
   const { assignment } = useSelector((state: any) => state.assignmentReducer);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const handleSave = () => {
+  
+  const handleSave = async () => {
     if (aid === "new") {
+      await assignmentClient.createNewAssignment(assignment);
       dispatch(addAssignment({
         ...assignment
       }));
     } else {
+      await assignmentClient.updateAssignment(assignment);
       dispatch(updateAssignment({
         ...assignment
       }));
--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -1,56 +1,15 @@
-import { Link, useLocation, useParams } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../styles.css";
-
-interface NavLink {
-  name: string;
-  isExternal?: boolean;
-  url?: string;
-}
-
 export default function CoursesNavigation() {
   const { pathname } = useLocation();
-  const { cid } = useParams();
-  
-  const links: NavLink[] = [
-    { name: "Home" },
-    { name: "Modules" },
-    { name: "Piazza", isExternal: true, url: "https://piazza.com/class" },
-    { name: "Zoom", isExternal: true, url: "https://northeastern.zoom.us/my/course" },
-    { name: "Assignments" },
-    { name: "Quizzes" },
-    { name: "Grades" },
-    { name: "People" }
-  ];
-  
-  const isActiveLink = (link: NavLink): boolean => {
-    const currentPath = pathname.split("/")[4];
-    return currentPath === link.name;
-  };
-
+  const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
       {links.map((link) => (
-        link.isExternal ? (
-          <a
-            key={link.name}
-            href={link.url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="list-group-item border border-0 text-danger"
-          >
-            {link.name}
-          </a>
-        ) : (
-          <Link
-            key={link.name}
-            to={`/Kambaz/Courses/${cid}/${link.name}`}
-            className={`list-group-item border border-0 ${
-              isActiveLink(link) ? "active" : "text-danger"
-            }`}
-          >
-            {link.name}
-          </Link>
-        )
+        <Link to={`./${link}`}
+        className={`list-group-item border border-0 ${pathname.includes(link) ? "active" : "text-danger"}`}>
+          {link}
+        </Link>
       ))}
     </div>
   );
