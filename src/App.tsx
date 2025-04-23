78a010bd13a2451c436e13c353f4dbaf6199d9f4
gollarahul091 <golla.ra@northeastern.edu>
2025-04-23 18:55:29 -0400

    Initial commit for branch a6

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+import Labs from './Labs';
+import Kambaz from './Kambaz';
+import LandingPage from './LandingPage';
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
+import store from "./Kambaz/store";
+import { Provider } from "react-redux";
+
+function App() {
+  return (
+    <HashRouter>
+      <Provider store={store}>
+        <div>
+          <Routes>
+            <Route path="/" element={<Navigate to="LandingPage" />} />
+            <Route path='/Labs/*' element={<Labs />} />
+            <Route path="/Kambaz/*" element={<Kambaz />} />
+            <Route path='/LandingPage' element={<LandingPage />} />
+          </Routes>
+        </div>
+      </Provider>
+    </HashRouter>
+  );
+}
+
+export default App;
---
+++ src/LandingPage/index.tsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import { FaFlask } from "react-icons/fa";
+import { FaBook } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+
+export default function LandingPage() {
+    return (
+        <div 
+            className="d-flex justify-content-center align-items-center" 
+            style={{ 
+                minHeight: "100vh", 
+                backgroundColor: "#212529" // Dark background
+            }}
+        >
+            <div className="text-center p-5 rounded shadow" style={{ backgroundColor: "white" }}>
+                <h1><b>Rahul Golla</b></h1>
+                <h3><span className="text-danger">NUID:</span> 002322414</h3>
+                <h3>CS5610 Spring 2025</h3>
+                <h3 className="mt-4 mb-3">Links</h3>
+                <div className="d-flex justify-content-center">
+                    <Link className="text-black link-underline link-underline-opacity-0" to={"/Labs"}>
+                        <button className="btn btn-lg btn-info me-2"><FaFlask className="me-2" />Labs</button>
+                    </Link>
+                    <Link to={"/Kambaz"}>
+                        <button className="btn btn-lg btn-info me-2"><FaBook className="me-2" />Kambaz</button>
+                    </Link>
+                    <Link to={"https://github.com/Rahul7569444/kambaz-react-web-app"}>
+                        <button className="btn btn-lg btn-info me-2"><FaGithub className="me-2" />Github</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
